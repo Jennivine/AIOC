2bf201e674c11c0a32336f2bee9ea21e2b7ac73d
Jennivine <jennivine@live.cn>
2019-06-23 18:19:19 +1000

    started word chains

---
+++ AIOC School of Excellence/Challenge Problems/2/3 Word Chains.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//adjacency list of characters and their children
+vector<int> charTree[3000000];
+
+int pointer;
+char input;
+
+int main () {
+	freopen("chains.in", "r", stdin);
+	freopen("chains.out","w",stdout);
+ 
+	scanf("%c", &input);
+	while (input != '.') {
+		if (input != '\n' && input != '\r') {
+			// read into the adj list
+
+
+		}
+		scanf("%c", &input);
+	}
+
+	// char with no 
+
+}
