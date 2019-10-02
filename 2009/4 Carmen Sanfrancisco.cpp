8100c4631c8bf357720ab59e9fd810d0ce9d7f83
Jennivine <jennivine@live.cn>
2019-10-02 11:48:13 +1000

    fixed Carmen Sanfrancisco, cabinet shuffle still not 100% solved

--- 2009/4 Carmen Sanfrancisco.cpp
+++ 2009/4 Carmen Sanfrancisco.cpp
@@ -5,63 +5,68 @@ int N,M;
 vector<pair<int,int> > cities[505];
 
 queue<int> possibleLocations;
-bool inQueue[505];
+int newPlaces[505];
+
 
 int main() {
-	freopen("wherein.txt", "r", stdin);
-	freopen("whereout.txt","w",stdout);
+    freopen("wherein.txt", "r", stdin);
+    freopen("whereout.txt","w",stdout);
 
-	scanf("%d %d", &N, &M);
+    scanf("%d %d", &N, &M);
 
-	for (int i=0; i<M;i++) {
-		int A,B,C;
-		scanf("%d %d %d", &A, &B, &C);
-		cities[A].push_back(make_pair(B,C));
-		cities[B].push_back(make_pair(A,C));
-	}
+    for (int i=0; i<M;i++) {
+        int A,B,C;
+        scanf("%d %d %d", &A, &B, &C);
+        cities[A].push_back(make_pair(B,C));
+        cities[B].push_back(make_pair(A,C));
+    }
 
-	int K;
-	scanf("%d", &K);
+    int K;
+    scanf("%d", &K);
 
-	for (int i=0; i<K; i++) {
-		int transport;
-		scanf("%d", &transport);
-		set<int> newPlaces;
-		
-		if (i == 0) {
-			//initialise queue
-			for (int i=1; i<=N; i++){
-				for (auto route:cities[i]){
-					if (route.second == transport) {
-						newPlaces.insert(route.first);
-					}
-				}
-			}
-		} else {
-			//create set for where CS could be next
-			while (!possibleLocations.empty()){
-				int place = possibleLocations.front();
-				possibleLocations.pop();
-				for (auto route:cities[place]) {
-					if (route.second == transport) {
-						newPlaces.insert(route.first);
-					}
-				}
-			}
-		}
-		//update queue
-		for (auto i:newPlaces) {
-			possibleLocations.push(i);
-		}
-	}
+    for (int i=0; i<K; i++) {
+        int transport;
+        scanf("%d", &transport);
+        
+        if (i == 0) {
+            //initialise queue
+            for (int i=1; i<=N; i++){
+                for (auto route:cities[i]){
+                    if (route.second == transport) {
+                        newPlaces[route.first] = 1;
+                    }
+                }
+            }
+        } else {
+            //create set for where CS could be next
+            while (!possibleLocations.empty()){
+                int place = possibleLocations.front();
+                possibleLocations.pop();
+                for (auto route:cities[place]) {
+                    if (route.second == transport) {
+                        newPlaces[route.first] = 1;
+                    }
+                }
+            }
+        }
+        //update queue
+        for (int i=1; i<=N; i++) {
+            if (newPlaces[i] == 1) {
+                possibleLocations.push(i);
+                newPlaces[i] = 0;
+            }
+        }
+    }
 
-	if (possibleLocations.empty()){
-		printf("Impossible\n");
-	} else {
-		while (!possibleLocations.empty()){
-			printf("%d\n", possibleLocations.front());
-			possibleLocations.pop();
-		}
-	}
+
+    if (possibleLocations.empty()){
+        printf("Impossible\n");
+    } else {
+        while (!possibleLocations.empty()){
+            printf("%d\n", possibleLocations.front());
+            possibleLocations.pop();
+        }
+    }
 }
+    
 		
--- AIOC School of Excellence/December Camp 2018/Data Structures (Alpha)/3 Cabinet Shuffle.py
+++ AIOC School of Excellence/December Camp 2018/Data Structures (Alpha)/3 Cabinet Shuffle.py
@@ -1,35 +1,96 @@
 with open("shufflein.txt") as I:
     N, K = map(int, I.readline().strip().split())
 
-    chairs = set(map(int, I.readline().strip().split()))
+    chairs = map(int, I.readline().strip().split())
     PMchair = min(chairs)
+
+    getIndex = {}
+    for i in range(len(chairs)):
+        getIndex[chairs[i]] = i
     
     ministers = map(int, I.readline().strip().split())
-    active = set(range(1,K+2))
 
-    info = [None]*(K+5)
-    for i in xrange(1, K+2):
-        info[i] = [i, ministers[i-1], False]
+    # make look-up table
+    dist_chair_to_chair = {}
+    for i in range(len(chairs)-1):
+        dist_chair_to_chair[chairs[i]] = chairs[i+1] - chairs[i]
+    dist_chair_to_chair[chairs[-1]] = N-chairs[-1]+chairs[0]
 
-
-while len(active) != 1:
-    sitting = set()
-    for m in active:
-        minister = info[m]
-        if minister[1] in chairs:
-            sitting.add(m)
-            chairs.remove(minister[1])
-            info[m][2] = True
-        else:
-            info[m][1] = (minister[1])%N +1
-
-    active -= sitting
-
-O = open("shuffleout.txt", "w")
-
-for minister in info:
-    if minister != None:
-        if minister[1] == PMchair:
-            O.write(str(minister[0]) + "\n")
+# helper functions
+def nextChairs(ministers):
+    ''' time-consuming but hopefully only get called onces'''
+    # maps each minister to the next chair at the start of the game
+    ls = []
+    counter = 0
+    for pos in ministers:
+        while counter < len(chairs) and chairs[counter] < pos:
+            counter += 1
             
-O.write(str(list(active)[0]) + "\n")
+        if counter < len(chairs):
+            ls.append(chairs[counter])
+        else:
+            ls.append(chairs[0])
+
+    return ls
+        
+
+def getDistance(minister, chair):
+    if minister <= chair:
+        distance = chair-minister
+    else:
+        distance = N-minister+chair
+
+    return distance
+
+def nextState(state):
+    global PM
+    eliminate = min(state)
+    shift = eliminate[0]
+    chair = eliminate[2]
+    newState = []
+
+    if chair == PMchair:
+        PM = eliminate[3]
+
+    # update dictionary
+    chair_num = getIndex[chair]
+    dist_chair_to_chair[chairs[(chair_num-1)%3]] += dist_chair_to_chair[chair]
+    
+
+    for data in state:
+        distance, minister_pos, chair_target, minister_num = data
+
+        minister_pos += shift
+        if minister_pos > N:
+            minister_pos -= N
+        distance -= shift
+
+        if distance != 0 and chair_target == chair:
+            distance += dist_chair_to_chair[chair_target]
+            chair_target = minister_pos + distance
+            if chair_target > N:
+                chair_target -= N
+            newState.append((distance, minister_pos, chair_target, minister_num))
+            
+    return newState
+
+# make data structure to represent the ministers
+PM = 0
+state = [] 
+next_chair = nextChairs(ministers)
+
+for i in range(len(ministers)):
+    distance = getDistance(ministers[i], next_chair[i]) # distance between minister and chair
+    state.append((distance, ministers[i], next_chair[i], i+1))
+'''
+# "simulate" the game, but not really?
+while len(state) > 1:
+    state = nextState(state)
+
+print PM, state[0][3]
+'''
+with open("shuffleout.txt", "w") as O:
+    O.write(str(PM) + "\n")
+    O.write(str(state[0][3]))
+
+
