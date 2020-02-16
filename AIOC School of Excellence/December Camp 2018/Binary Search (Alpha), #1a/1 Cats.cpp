28c1dbf4674f3fe4a12ffe398107fa9353d2d61e
Jennivine <45113203+Jennivine@users.noreply.github.com>
2020-02-17 10:05:33 +1100

    Finally debugged Cats!

--- AIOC School of Excellence/December Camp 2018/Binary Search (Alpha), #1a/1 Cats.cpp
+++ AIOC School of Excellence/December Camp 2018/Binary Search (Alpha), #1a/1 Cats.cpp
@@ -4,78 +4,61 @@ using namespace std;
 int N; //number of nodes
 int P; //number of edges
 
-vector<int> ANU[505];
-pair<int, pair<int, int> > edges[100005]; // <K,<X,Y> >
+vector<pair<int, int> > ANU[505];
 int checked[505];
 
-int DFS(int x, int y);
+int DFS(int x, int y, int w);
 
 int main() {
 	freopen("catsin.txt","r",stdin);
-	//freopen("catsout.txt","w",stdout);
+	freopen("catsout.txt","w",stdout);
 	scanf("%d", &N);
 	scanf("%d", &P);
 
 	for (int i=0; i<P; i++) {
 		int X,Y,K;
 		scanf("%d %d %d", &X, &Y, &K);
-
-		edges[i] = make_pair(K,make_pair(X,Y));
+		ANU[X].push_back(make_pair(Y, K));
+		ANU[Y].push_back(make_pair(X, K));
 	}
 
-	sort(edges, edges+P);
-	
 	int B,C;
 	scanf("%d %d", &B, &C);	
 
 	int lo = 0;
 	int hi = 1e9;
+	
 	while (lo != hi) {
 		//do binary search on minimum maximum cost
 		int mid = (lo+hi)/2;	
 		
-		//construct graph using only edges <= mid
-		int i = 0;
-		while (edges[i].first <= mid && i<N){
-			int x = edges[i].second.first;
-			int y = edges[i].second.second;
-			ANU[x].push_back(y);
-			ANU[y].push_back(x);
-			i++;
-		}
-		
-		/*debug
-		printf("mid:%d\n",mid);
-		for (int i=0; i<N; i++){
-			if (ANU[i].empty()) continue;
-			for (auto n:ANU[i]){
-				printf("%d %d\n", i,n);
-			}
-		}*/
-
-		if (DFS(B,C)){
+		if (DFS(B, C, mid)){
 			hi = mid;
 		} else {
 			lo = mid+1;
 		}
 
 		//cleanup
-		for (int i=0; i<N; i++) {
-			ANU[i].clear();
+		for (int i=1; i<=N; i++) {
 			checked[i] = 0;
 		}
 	}
-
+	
 	printf("%d\n", lo);
 }
 
-int DFS(int x, int y) {
-	//returns 1 is there's a path, 0 otherwise
+int DFS(int x, int y, int w) {
+	// returns 1 is there's a path, 0 otherwise using only paths of weight w
 	if (checked[x]) {return 0;}
 	if (x == y) {return 1;}
+	
 	checked[x] = 1;
+	
 	for (auto n:ANU[x]){
-		if (DFS(n,y)) {return 1;}
+		int node = n.first;
+		int cats = n.second;
+		if (cats <= w && DFS(node,y,w)) {return 1;}
 	}
+	
 	return 0;
 }
